40fa8dcd2a04c59c34324c8dea5daf65adf230a4
alain-ocpm <alain.benoit@ocpm.qc.ca>
2016-08-22 16:59:57 -0400

    First major commit

---
+++ template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function oidp_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function oidp_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function oidp_preprocess_node(&$variables) {
+}
