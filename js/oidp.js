dcec9128f9d70d5adbb1e67dcb5dc5a9d1f4c737
alain-ocpm <alain.benoit@ocpm.qc.ca>
2016-09-15 16:47:07 -0400

    New modules

---
+++ js/oidp.js
@@ -0,0 +1,35 @@
+(function($) {
+  //Fade In Effect
+  var fadeIn = function() {
+    $("body").css("opacity", "0");
+    setTimeout(function(){ $("body").fadeTo(250, 1); }, 500);
+  }
+
+  //Schedule
+  var schedule = function() {
+
+    $(".tab-title a").on("click", function (event) {
+      var targetPanel = $(this).attr("href");
+
+      $(".tabs-content .content").css("display", "none");
+      $(targetPanel).css("display", "block");
+      $(targetPanel).css("opacity", "0");
+      $(targetPanel).fadeTo(750, 1);
+     })
+
+    //Variable a changer pour la journée
+    var currentDay = "jour1";
+    currentDay = "#" + currentDay;
+
+    if($(currentDay).length) {
+      $(currentDay).addClass("active");
+    }
+  };
+
+  var init = function() {
+    //fadeIn();
+    schedule();
+  }
+
+  $(document).ready(init);
+})(jQuery)
