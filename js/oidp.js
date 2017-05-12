307cd5d56b015e55403f39961b780b3718ea294f
alain-ocpm <alain.benoit@ocpm.qc.ca>
2017-05-12 12:19:32 -0400

    Slider arrows fix

--- js/oidp.js
+++ js/oidp.js
@@ -1,47 +1,11 @@
 $(document).ready(function(){
-  $(document).foundation();
+  window.addEventListener("load", function(event) {
+    console.log("lel");
+    $(document).foundation();
+  });
+
   $(".tabs-content-slide-rail").slick({
     slidesToShow: 3,
     slidesToScroll: 1
   });
 });
-
-
-
-
-
-/*(function($) {
-  //Fade In Effect
-  var fadeIn = function() {
-    $("body").css("opacity", "0");
-    setTimeout(function(){ $("body").fadeTo(250, 1); }, 500);
-  }
-
-  //Schedule
-  var schedule = function() {
-
-    $(".tab-title a").on("click", function (event) {
-      var targetPanel = $(this).attr("href");
-
-      $(".tabs-content .content").css("display", "none");
-      $(targetPanel).css("display", "block");
-      $(targetPanel).css("opacity", "0");
-      $(targetPanel).fadeTo(750, 1);
-     })
-
-    //Variable a changer pour la journée
-    var currentDay = "jour1";
-    currentDay = "#" + currentDay;
-
-    if($(currentDay).length) {
-      $(currentDay).addClass("active");
-    }
-  };
-
-  var init = function() {
-    //fadeIn();
-    schedule();
-  }
-
-  $(document).ready(init);
-})(jQuery)*/
